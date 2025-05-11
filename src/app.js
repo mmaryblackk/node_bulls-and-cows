f001899ef27e7b464a836a2a4011e7902ab2f648
Maryna Kravchuk <marinalinkova02@gmail.com>
2025-05-11 15:03:05 +0300

    add solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,38 @@
+/* eslint-disable no-console */
 'use strict';
 
-// Write your code here
+import readline from 'readline';
+import { generateRandomNumber } from './modules/generateRandomNumber.js';
+import { checkIsValidUserInput } from './modules/checkIsValidUserInput.js';
+import { getBullsAndCows } from './modules/getBullsAndCows.js';
+
+const terminal = readline.createInterface(process.stdin, process.stdout);
+
+const firstQuestion = 'Try to guess 4-digit number that I am thinking of  ';
+const errorQuestion = 'The number must be 4-digit and does not start with 0  ';
+const continueMessage = 'Try one more guess!  ';
+const endMessage = 'Congrats!!! You guessed it!!!';
+
+const secretNum = generateRandomNumber();
+
+const askQuestion = (question) => {
+  terminal.question(`${question}`, (input) => {
+    const isInputValid = checkIsValidUserInput(input);
+
+    if (!isInputValid) {
+      askQuestion(errorQuestion);
+    } else {
+      const result = getBullsAndCows(input, secretNum);
+
+      if (result.bulls === 4) {
+        console.log(result, endMessage);
+        terminal.close();
+      } else {
+        console.log(result);
+        askQuestion(continueMessage);
+      }
+    }
+  });
+};
+
+askQuestion(firstQuestion);
